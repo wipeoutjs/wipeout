a520e4c68fa080e4d89e0cf6552bbbf6ee729d05
Shane Connon <shane.connon@gmail.com>
2014-12-15 19:51:33 -0700

    exposing observable obj and array

--- src/base/watch.js
+++ src/base/watch.js
@@ -9,11 +9,11 @@ Class("wipeout.base.watch", function () {
         ///<param name="object" type="Object" optional="true">The object to add functionality to. Defaults to {}</param>
         ///<returns type="Object">The amended object</returns>
         
+        if (!arguments.length) object = {};
+        
         if (watch.canWatch(object))
             return object;
-        
-        object = object || {};
-        
+                
         var woBag = new wipeout.base.watched().__woBag;
         var _watch = wipeout.base.watched.createWatchFunction(woBag, {});
         var observe = wipeout.base.watched.createObserveFunction(woBag, _watch);
--- tools/build.js
+++ tools/build.js
@@ -1,5 +1,15 @@
 window.wipeout = wipeout;
 window.wo = {};
+
+window.wo.object = wipeout.base.watch;
+window.wo.array = function (initialValues) {
+    ///<summary>Create a new observable array</summary>
+    ///<param name="initialValues" type="Array" optional="true">The initial values of the array</param>
+    ///<returns type="wo.base.array">The observable array</returns>
+    
+    return new wipeout.base.array(initialValues);
+};
+
 enumerateObj(wipeout.base, function(item, i) {
     window.wo[i] = item;
 });
