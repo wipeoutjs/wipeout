7ba4eea5f3cb5901a8a65d58ca9ba02ded6e7d98
Shane Connon <shane.connon@gmail.com>
2015-01-29 16:56:10 -0700

    temp

--- src/change/objectObserveObjectHandler.js
+++ src/change/objectObserveObjectHandler.js
@@ -23,11 +23,18 @@ Class("wipeout.change.objectObserveObjectHandler", function () {
     });
     
     objectObserveObjectHandler.prototype.disposeOfObserve = function (property, observeCallback, disposeCallback) {
+        this.onNextPropertyChange(property, function (change) {
+            observeCallback.changeValidator.afterLastChange(change, disposeCallback);
+        });
+    };
+        
+    objectObserveObjectHandler.prototype.onNextPropertyChange = function(property, callback) {
         var cb = (function (changes) {            
             for (var i = 0, ii = changes.length; i < ii; i++) {                
                 if (wipeout.change.objectHandler.changeIsForThisProperty(property, changes[i])) {                    
                     (this.forArray ? Array : Object).unobserve(this.forObject, cb);
-                    observeCallback.changeValidator.afterLastChange(changes[i], disposeCallback);
+                    callback(changes[i]);
+                    return;
                 }
             }            
         }).bind(this);
@@ -69,7 +76,52 @@ Class("wipeout.change.objectObserveObjectHandler", function () {
             }, _this);
         };
             
-        (this.forArray ? Array : Object).observe(this.forObject, tempSubscription);        
+        (this.forArray ? Array : Object).observe(this.forObject, tempSubscription);     
+        this.onNextPropertyChange(property, function (change) {
+            
+            _this.extraCallbacks--;
+            
+            // was disposed of
+            if (!_this || _this.__subscription) {
+                return;
+            }
+
+            callback.changeValidator.registerFirstChange(change);
+            callbackList.push(callback);
+            sortCallback();
+            
+            
+            
+            
+            // was disposed of
+            if (!_this.__subscription) {
+                Object.unobserve(_this.forObject, tempSubscription);
+                return;
+            }
+                        
+            // has been disposed of, do nothing
+            if (!_this) return;
+                     
+            var firstChangeDone = false;
+            enumerateArr(changes, function(change) {
+                
+                if (!firstChangeDone && wipeout.change.objectHandler.changeIsForThisProperty(property, change)) {
+                    Object.unobserve(_this.forObject, tempSubscription);
+                    _this.extraCallbacks--;
+                    firstChangeDone = true;
+                    
+                    callback.changeValidator.registerFirstChange(change);
+                    callbackList.push(callback);
+                    sortCallback();
+                }
+                
+                // record change so that another subscription will not act on it
+                if (_this.registeredChanges.indexOf(change) !== -1) return;
+                _this.registeredChanges.push(change);
+                
+                _this.registerChange(change);
+            }, _this);
+        });
         this.extraCallbacks++;
         
         return function () {
