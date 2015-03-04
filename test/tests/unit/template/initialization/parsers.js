dcb3ef29e62323c5fe4d6dba650b05d2c7f9c767
Shane Connon <shane.connon@gmail.com>
2015-03-03 20:30:43 -0700

    tests

---
+++ test/tests/unit/template/initialization/parsers.js
@@ -0,0 +1,76 @@
+
+module("wipeout.template.initialization.parsers", {
+    setup: function() {
+    },
+    teardown: function() {
+    }
+});
+
+testUtils.testWithUtils("template", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker("aval"), "aval");
+});
+
+testUtils.testWithUtils("viewModelId", null, true, function(methods, classes, subject, invoker) {
+    // arrange
+	var parent = new wipeout.viewModels.view(), _this = {};
+	
+    // act    
+    // assert   
+    strictEqual(invoker("aval", null, {$parent: parent, $this: _this}), "aval");
+    strictEqual(parent.templateItems.aval, _this);
+});
+
+testUtils.testWithUtils("string", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker("aval"), "aval");
+});
+
+testUtils.testWithUtils("bool", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker(" tRue "), true);
+});
+
+testUtils.testWithUtils("int", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker(" 44 "), 44);
+});
+
+testUtils.testWithUtils("float", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker(" 44.55 "), 44.55);
+});
+
+testUtils.testWithUtils("regexp", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    ok(invoker("^Hello$").test("Hello"));
+});
+
+testUtils.testWithUtils("date", null, true, function(methods, classes, subject, invoker) {
+    // arrange    
+    // act    
+    // assert   
+    strictEqual(invoker("2012/01/01") - new Date("2012/01/01"), 0);
+});
+    
+testUtils.testWithUtils("json", null, true, function(methods, classes, subject, invoker) {
+    // arrange
+    // act    
+    var json = invoker('{"val1":8,"val2":"3"}')
+    
+    // assert   
+    strictEqual(json.val1, 8);
+    strictEqual(json.val2, "3");
+});
