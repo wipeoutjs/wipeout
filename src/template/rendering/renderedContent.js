36517c4f39cbf2deb1186b25d6a1df7b80022203
Shane Connon <shane.connon@gmail.com>
2015-03-11 14:05:32 -0600

    refactored render array, pt 2

--- src/template/rendering/renderedContent.js
+++ src/template/rendering/renderedContent.js
@@ -29,6 +29,27 @@ Class("wipeout.template.rendering.renderedContent", function () {
 		this.openingTag.nodeValue = " " + name + " ";
 		this.closingTag.nodeValue = " /" + name + " ";
 	};
+	    
+    renderedContent.prototype.renderArray = function (array) {
+        
+        // if a previous request is pending, cancel it
+        if (this.asynchronous) {
+            this.asynchronous.cancel();
+			delete this.asynchronous;
+		}
+                
+        this.unRender();
+        
+		var ra = new wipeout.template.rendering.renderedArray(array, this);
+        this.disposeOfBindings = ra.dispose.bind(ra);
+		
+		ra.render([{
+			type: "splice",
+			addedCount: array.length,
+			removed: [],
+			index: 0
+		}]);
+	};
 	
     renderedContent.prototype.render = function (object, arrayIndex) {
 		
