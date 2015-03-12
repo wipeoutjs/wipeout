e244850fc1cdaae4cdbe11afc4a4a5f9fa8e3ea4
Shane Connon <shane.connon@gmail.com>
2015-03-11 21:46:29 -0600

    tests

--- test/tests/unit/viewModels/view.js
+++ test/tests/unit/viewModels/view.js
@@ -75,3 +75,130 @@ testUtils.testWithUtils("_onModelRoutedEvent", "", false, function(methods, clas
     
     // assert    
 });
+
+/*testUtils.testWithUtils("getParent", "don't include shareParentScope", false, function(methods, classes, subject, invoker) {
+    // arrange
+    var expected = {};
+    var parentElement = {};
+    classes.mock("wipeout.utils.ko.parentElement", function() {
+        strictEqual(arguments[0], subject.__woBag.rootHtmlElement);
+        return parentElement;
+    }, 1);
+    
+    classes.mock("wipeout.utils.html.getViewModel", function() {
+        strictEqual(arguments[0], parentElement);
+        return {
+            shareParentScope: true,
+            getParent: function() {
+                ok(!arguments[0]);
+                return expected;
+            }
+        };
+    });
+    
+    subject.__woBag = {
+        rootHtmlElement: {}
+    };
+    
+    // act
+    var actual = invoker(false);
+    
+    // assert
+    strictEqual(actual, expected);
+});
+
+testUtils.testWithUtils("getParent", "include shareParentScope", false, function(methods, classes, subject, invoker) {
+    // arrange
+    var expected = {};
+    var parentElement = {};
+    classes.mock("wipeout.utils.ko.parentElement", function() {
+        strictEqual(arguments[0], subject.__woBag.rootHtmlElement);
+        return parentElement;
+    }, 1);
+    
+    classes.mock("wipeout.utils.html.getViewModel", function() {
+        strictEqual(arguments[0], parentElement);
+        return expected;
+    });
+    
+    subject.__woBag = {
+        rootHtmlElement: {}
+    };
+    
+    // act
+    var actual = invoker(false);
+    
+    // assert
+    strictEqual(actual, expected);
+});*/
+
+testUtils.testWithUtils("unRegisterRoutedEvent", "no event", false, function(methods, classes, subject, invoker) {
+    // arrange
+    subject.$routedEventSubscriptions = [];
+    
+    // act
+    var actual = invoker();
+    
+    // assert
+    ok(!actual);
+});
+
+testUtils.testWithUtils("unRegisterRoutedEvent", "no event", false, function(methods, classes, subject, invoker) {
+    // arrange
+    var callback = {};
+    var context = {};
+    var routedEvent = {};
+    subject.$routedEventSubscriptions = [{
+		routedEvent: routedEvent,
+		event: {
+			unRegister: methods.method([callback, context])
+		}
+	}];
+    
+    // act
+    var actual = invoker(routedEvent, callback, context);
+    
+    // assert
+    ok(actual);
+});
+
+testUtils.testWithUtils("registerRoutedEvent", "event exists", false, function(methods, classes, subject, invoker) {
+    // arrange
+    var expected = {};
+    var callback = {};
+    var context = {};
+    var routedEvent = {};
+    subject.$routedEventSubscriptions = [{
+		routedEvent: routedEvent,
+		event: {
+			register: methods.method([callback, context], expected)
+		}
+	}];
+    
+    // act
+    var actual = invoker(routedEvent, callback, context);
+    
+    // assert
+    strictEqual(actual, expected);
+});
+
+testUtils.testWithUtils("registerRoutedEvent", "new event", false, function(methods, classes, subject, invoker) {
+    // arrange
+    var expected = {};
+    function callback() {};
+    var context = {};
+    var routedEvent = {};
+    subject.$routedEventSubscriptions = [];
+    
+    // act
+    var actual = invoker(routedEvent, callback, context);
+    
+    // assert
+    strictEqual(subject.$routedEventSubscriptions.length, 1);
+    strictEqual(actual.dispose.constructor, Function);
+});
+
+testUtils.testWithUtils("triggerRoutedEvent", "no test here. see integration tests instead", false, function(methods, classes, subject, invoker) {
+    // arrange
+    ok(true);
+});
