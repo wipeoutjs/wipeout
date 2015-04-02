0d05ac0fa3502d1308af0957eb1f6eb2429dbec1
Shane Connon <shane.connon@gmail.com>
2015-04-02 15:26:23 -0600

    init model

--- src/wo.js
+++ src/wo.js
@@ -11,19 +11,27 @@ window.wo = function (model, htmlElement) {
     else if (typeof htmlElement === "string")
         htmlElement = document.getElementById(htmlElement);
 
-	if (wipeout.utils.viewModels.getViewModelConstructor(htmlElement))
-		output.registerDisposable(new wipeout.template.rendering.viewModelElement(htmlElement));
-	else
+	if (wipeout.utils.viewModels.getViewModelConstructor(htmlElement)) {
+		var vme = new wipeout.template.rendering.viewModelElement(htmlElement);
+		if (model != null)
+			vme.createdViewModel.model = model;
+		output.registerDisposable(vme);
+	} else {
 		enumerateArr(wipeout.utils.obj.copyArray(htmlElement.getElementsByTagName("*")), function (element) {
 
 			// element may have been removed since get all elements
-			if (htmlElement.contains(element) && wipeout.utils.viewModels.getViewModelConstructor(element))
-				output.registerDisposable(new wipeout.template.rendering.viewModelElement(element));
+			if (htmlElement.contains(element) && wipeout.utils.viewModels.getViewModelConstructor(element)) {
+				var vme = new wipeout.template.rendering.viewModelElement(element);
+				if (model != null)
+					vme.createdViewModel.model = model;
+				output.registerDisposable(vme);
+			}
 		});
+	}
 	
 	return output;
 };
 
 window.addEventListener("load", function () {
-    window.wo(null); //TODO: model
+    window.wo();
 });
