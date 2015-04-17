4f44ecd75350ce7263e8dd0b9b6a4d0599d100fe
Shane Connon <shane.connon@gmail.com>
2015-04-17 12:50:06 -0600

    tests

--- test/tests/unit/template/initialization/propertySetter.js
+++ test/tests/unit/template/initialization/propertySetter.js
@@ -5,37 +5,8 @@ module("wipeout.template.initialization.propertySetter", {
     teardown: function() {
     }
 });
-/*
-testUtils.testWithUtils("constructor", "parser", false, function(methods, classes, subject, invoker) {
-    // arrange
-	var val = {}, name = {};
-	subject._super = methods.method([name, val, "i"]);
-    
-	// act
-	invoker(name, val, "i");
-});
 
-testUtils.testWithUtils("getParser", "has parser", false, function(methods, classes, subject, invoker) {
-    // arrange
-	var p = subject.parser = {};
-    
-	// act
-    // assert
-    strictEqual(invoker(), p);
-});
-
-testUtils.testWithUtils("getParser", "has global parser", false, function(methods, classes, subject, invoker) {
-    // arrange
-	var vm = wipeout.base.bindable.extend(function () { this._super(); });
-	vm.addGlobalParser("aaaa", "s");
-    subject.name = "aaaa";
-	
-	// act
-    // assert
-    strictEqual(invoker(new vm()), wipeout.template.initialization.parsers.s);
-});
-
-testUtils.testWithUtils("getValue", "has val", false, function(methods, classes, subject, invoker) {
+testUtils.testWithUtils("value", "has val", false, function(methods, classes, subject, invoker) {
     // arrange
 	subject._valueAsString = "LKBLKNLBLJB";
     
@@ -44,7 +15,7 @@ testUtils.testWithUtils("getValue", "has val", false, function(methods, classes,
     strictEqual(invoker(), subject._valueAsString);
 });
 
-testUtils.testWithUtils("getValue", "no val", false, function(methods, classes, subject, invoker) {
+testUtils.testWithUtils("value", "no val", false, function(methods, classes, subject, invoker) {
     // arrange
 	var val = "KJBKJBKJBKJBJ";
 	subject._super = function () {
@@ -59,51 +30,19 @@ testUtils.testWithUtils("getValue", "no val", false, function(methods, classes, 
     strictEqual(val, subject._valueAsString);
 });
 
-testUtils.testWithUtils("parseOrExecute", "useRawXmlValue", false, function(methods, classes, subject, invoker) {
+testUtils.testWithUtils("onPropertyChanged", "no val", false, function(methods, classes, subject, invoker) {
     // arrange
-	var output = {};
-	subject._value = {}, subject.name = {};
-	var vm = {}, rc = {}, parser = methods.method([subject._value, subject.name, rc], output);
-	subject.getParser = methods.method([vm], parser);
-    parser.useRawXmlValue = true;
+	subject.propertyOwner = {aaa:{}};
+	subject.name = "aaa";
+	var obs = {}, cb = methods.method([undefined, subject.propertyOwner.aaa]);
+	subject.primed = methods.method();
+	subject._caching = [];
+    classes.mock("obsjs.tryObserve", function () {
+		methods.method([subject.propertyOwner, subject.name, cb]).apply(null, arguments);
+		return obs;
+	});
 	
 	// act
-	var op = invoker(vm, rc);
-	
     // assert
-    strictEqual(op, output);
+    ok(invoker(cb, true));
 });
-
-testUtils.testWithUtils("parseOrExecute", "parser", false, function(methods, classes, subject, invoker) {
-    // arrange
-	var value = {}, output = {};
-	subject.getValue = methods.method([], value)
-	subject.name = {};
-	var vm = {}, rc = {}, parser = methods.method([value, subject.name, rc], output);
-	subject.getParser = methods.method([vm], parser);
-	
-	// act
-	var op = invoker(vm, rc);
-	
-    // assert
-    strictEqual(op, output);
-});
-
-testUtils.testWithUtils("parseOrExecute", "no parser", false, function(methods, classes, subject, invoker) {
-    // arrange
-	var ga = {}, op = {}, built = {
-		apply: methods.method([null, ga], op)
-	};
-	subject.getParser = methods.method();
-	subject.build = methods.method([], built);
-	
-	var vm = {}, rc = {
-		asGetterArgs: methods.method([], ga)
-	};
-	
-	// act
-	var output = invoker(vm, rc);
-	
-    // assert
-    strictEqual(op, output);
-});*/
--- test/tests/unit/template/setter.js
+++ test/tests/unit/template/setter.js
@@ -103,3 +103,81 @@ testUtils.testWithUtils("getValue", null, false, function(methods, classes, subj
 	// assert
 	strictEqual(subject._value = {}, invoker());
 });*/
+
+/*testUtils.testWithUtils("constructor", "parser", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var val = {}, name = {};
+	subject._super = methods.method([name, val, "i"]);
+    
+	// act
+	invoker(name, val, "i");
+});
+
+testUtils.testWithUtils("getParser", "has parser", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var p = subject.parser = {};
+    
+	// act
+    // assert
+    strictEqual(invoker(), p);
+});
+
+testUtils.testWithUtils("getParser", "has global parser", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var vm = wipeout.base.bindable.extend(function () { this._super(); });
+	vm.addGlobalParser("aaaa", "s");
+    subject.name = "aaaa";
+	
+	// act
+    // assert
+    strictEqual(invoker(new vm()), wipeout.template.initialization.parsers.s);
+});*/
+
+/*testUtils.testWithUtils("parseOrExecute", "useRawXmlValue", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var output = {};
+	subject._value = {}, subject.name = {};
+	var vm = {}, rc = {}, parser = methods.method([subject._value, subject.name, rc], output);
+	subject.getParser = methods.method([vm], parser);
+    parser.useRawXmlValue = true;
+	
+	// act
+	var op = invoker(vm, rc);
+	
+    // assert
+    strictEqual(op, output);
+});
+
+testUtils.testWithUtils("parseOrExecute", "parser", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var value = {}, output = {};
+	subject.getValue = methods.method([], value)
+	subject.name = {};
+	var vm = {}, rc = {}, parser = methods.method([value, subject.name, rc], output);
+	subject.getParser = methods.method([vm], parser);
+	
+	// act
+	var op = invoker(vm, rc);
+	
+    // assert
+    strictEqual(op, output);
+});
+
+testUtils.testWithUtils("parseOrExecute", "no parser", false, function(methods, classes, subject, invoker) {
+    // arrange
+	var ga = {}, op = {}, built = {
+		apply: methods.method([null, ga], op)
+	};
+	subject.getParser = methods.method();
+	subject.build = methods.method([], built);
+	
+	var vm = {}, rc = {
+		asGetterArgs: methods.method([], ga)
+	};
+	
+	// act
+	var output = invoker(vm, rc);
+	
+    // assert
+    strictEqual(op, output);
+});*/
