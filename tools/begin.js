9469934857699f54ad8cd972b71400f00e959e81
Shane Connon <shane.connon@gmail.com>
2015-04-25 15:59:27 -0600

    getViewModel and wo(...) refactor

--- tools/begin.js
+++ tools/begin.js
@@ -3,8 +3,14 @@
     
     window.wipeout = {};
 	
-	function warn (warning) {
-		if (wipeout.settings.displayWarnings)
-			console.warn(warning);
+	function warn (warning, data) {
+		if (wipeout.settings.displayWarnings) {
+			warning += "\n\nTo disable warnings globally you can set \"wipeout.settings.displayWarnings\" to false.";
+			
+			console.warn(data ? {
+				message: warning,
+				data: data
+			} : warning);
+		}
 	}
     
