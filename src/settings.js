b6741ac47e8e1a2b0dbb755f9b60198b114fd013
Shane Connon <shane.connon@gmail.com>
2015-05-25 13:23:23 -0700

    issue #87, added  prop

--- src/settings.js
+++ src/settings.js
@@ -1,13 +1,21 @@
+var bindingStrategies = {
+    onlyBindObservables: 0,
+    bindNonObservables: 1,
+    createObservables: 2
+};
+
 Class("wipeout.settings", function() {
     function settings (settings) {
         ///<summary>Change all settings</summary>
         ///<param name="settings" type="Object">A dictionary of new settings</param>
 		
         enumerateObj(wipeout.settings, function(a,i) {
+            if (i === "bindingStrategies") return;
             delete wipeout.settings[i];
         });
         
         enumerateObj(settings, function(setting, i) {
+            if (i === "bindingStrategies") return;
             wipeout.settings[i] = setting;
         });        
     }
@@ -15,6 +23,14 @@ Class("wipeout.settings", function() {
     settings.asynchronousTemplates = true;
     settings.displayWarnings = true;
     settings.useElementClassName = false;
+    
+    settings.bindingStrategies = {
+        onlyBindObservables: 0,
+        bindNonObservables: 1,
+        createObservables: 2
+    };
+    
+    settings.bindingStrategy = settings.bindingStrategies.createObservables;
 	
     return settings;
 });
