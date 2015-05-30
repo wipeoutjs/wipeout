7e2feb27d192075f5f1b792d2260d025b15abbda
Shane Connon <shane.connon@gmail.com>
2015-05-29 18:09:14 -0700

    if tests, no functionality

---
+++ test/tests/integration/template/rendering/htmlAttributes/if.js
@@ -0,0 +1,23 @@
+module("integration: wipeout.template.initialization.htmlAttributes.wo-if", {
+    setup: integrationTestSetup,
+    teardown: integrationTestTeardown
+});
+/*
+test("if", function() {
+    
+    // arrange
+    application.model = false;
+    
+    // act
+    application.setTemplate = '<div wo-if="$model">\
+    <span id="woIfTest"></span>\
+</div>';
+    
+    application.onRendered = function () {
+        // assert
+        ok(!document.getElementById("woIfTest"));
+        start();
+    }
+    
+    stop();
+});*/
